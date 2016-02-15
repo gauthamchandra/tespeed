51c48c438c8666071ecb1676fae62bdbd6fc21f6
Gautham Chandra <gautham.chandra@live.com>
2016-02-15 16:53:06 -0500

    Do prelim changes for publishing to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'tespeed',
+  packages = ['tespeed'],
+  version = '1.1',
+  description = 'Terminal network speed test that uses servers from Speedtest.net. It uses nearest test server but can also use one manually specified by the user',
+  author = 'Janis Jansons',
+  author_email = 'janis.jansons@janhouse.lv',
+  url = 'https://github.com/gauthamchandra/tespeed',
+  download_url = 'https://github.com/Janhouse/tespeed/tarball/1.1',
+  keywords = ['speedtest.net', 'network', 'speed'],
+  classifiers = []
+)
